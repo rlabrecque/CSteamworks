9de5935926e6bb5b54690132850620c9df6df7d6
Riley Labrecque <git@rileylabrecque.com>
2013-11-09 22:17:01 -0600

    Wrapped Steam_BGetCallback and friends from steamclient.*

--- src/steam_api.cpp
+++ src/steam_api.cpp
@@ -206,3 +206,253 @@ SB_API const uint8 *EncryptedAppTicket_GetUserVariableData(uint8 *rgubTicketDecr
 	return SteamEncryptedAppTicket_GetUserVariableData(rgubTicketDecrypted, cubTicketDecrypted, pcubUserData);
 }
 #endif // 0
+
+typedef bool (__cdecl * BGetCallback_t)(HSteamPipe, CallbackMsg_t *);
+typedef void (__cdecl * FreeLastCallback_t)(HSteamPipe);
+typedef bool (__cdecl * GetAPICallResult_t)(HSteamPipe, SteamAPICall_t, void *, int, int, bool *);
+
+#if defined( _WIN32 )
+#define WINVER 0x0501 //WinXP
+#define _WIN32_WINNT 0x0501 //WinXP
+#define WIN32_LEAN_AND_MEAN
+#define STRICT
+#define NOGDICAPMASKS
+#define NOVIRTUALKEYCODES
+#define NOWINMESSAGES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
+#define NOKERNEL
+#define NOUSER
+#define NONLS
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#include <windows.h> // God Damn it Windows.
+#include <stdio.h> // fprintf
+
+SB_API bool __cdecl Steam_BGetCallback2(HSteamPipe hSteamPipe, CallbackMsg_t *pCallbackMsg) {
+	static BGetCallback_t _Steam_BGetCallback = NULL;
+	if(!_Steam_BGetCallback) {
+		HMODULE hSteamClient = GetModuleHandleA("steamclient.dll");
+		if(hSteamClient) {
+			_Steam_BGetCallback = reinterpret_cast<BGetCallback_t>(GetProcAddress(hSteamClient, "Steam_BGetCallback"));
+
+			if(!_Steam_BGetCallback) {
+				fprintf(stderr, "[CSteamworks] GetProcAddress failed to find Steam_BGetCallback\n");
+				return false;
+			}
+		}
+		else {
+			fprintf(stderr, "[CSteamworks] GetModuleHandleA failed opening steamclient.dll\n");
+			return false;
+		}
+	}
+	
+	return _Steam_BGetCallback(hSteamPipe, pCallbackMsg);
+}
+
+SB_API void __cdecl Steam_FreeLastCallback2(HSteamPipe hSteamPipe) {
+	static FreeLastCallback_t _Steam_FreeLastCallback = NULL;
+	if(!_Steam_FreeLastCallback) {
+		HMODULE hSteamClient = GetModuleHandleA("steamclient.dll");
+		if(hSteamClient) {
+			_Steam_FreeLastCallback = reinterpret_cast<FreeLastCallback_t>(GetProcAddress(hSteamClient, "Steam_FreeLastCallback"));
+			
+			if(!_Steam_FreeLastCallback) {
+				fprintf(stderr, "[CSteamworks] GetProcAddress failed to find Steam_FreeLastCallback\n");
+				return;
+			}
+		}
+		else {
+			fprintf(stderr, "[CSteamworks] GetModuleHandleA failed opening steamclient.dll\n");
+			return;
+		}
+	}
+	
+	return _Steam_FreeLastCallback(hSteamPipe);
+}
+
+SB_API bool __cdecl Steam_GetAPICallResult2(HSteamPipe hSteamPipe, SteamAPICall_t hSteamAPICall, void* pCallback, int cubCallback, int iCallbackExpected, bool* pbFailed) {
+	static GetAPICallResult_t _Steam_GetAPICallResult = NULL;
+	if(!_Steam_GetAPICallResult) {
+		HMODULE hSteamClient = GetModuleHandleA("steamclient.dll");
+		if(hSteamClient) {
+			_Steam_GetAPICallResult = reinterpret_cast<GetAPICallResult_t>(GetProcAddress(hSteamClient, "Steam_GetAPICallResult"));
+			
+			if(!_Steam_GetAPICallResult) {
+				fprintf(stderr, "[CSteamworks] GetProcAddress failed to find Steam_GetAPICallResult\n");
+				return false;
+			}
+		}
+		else {
+			fprintf(stderr, "[CSteamworks] GetModuleHandleA failed opening steamclient.dll\n");
+			return false;
+		}
+	}
+
+	return _Steam_GetAPICallResult(hSteamPipe, hSteamAPICall, pCallback, cubCallback, iCallbackExpected, pbFailed);
+}
+
+#elif defined( __APPLE__ )
+
+#include <dlfcn.h> // dlopen, dlsym, dlclose
+#include <stdio.h> // fprintf
+
+SB_API bool Steam_BGetCallback2(HSteamPipe hSteamPipe, CallbackMsg_t *pCallbackMsg) {
+	static BGetCallback_t _Steam_BGetCallback = NULL;
+	if(!_Steam_BGetCallback) {
+        _Steam_BGetCallback = reinterpret_cast<BGetCallback_t>(dlsym(RTLD_DEFAULT, "Steam_BGetCallback"));
+            
+        if(!_Steam_BGetCallback) {
+            fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_BGetCallback\n");
+            return false;
+        }
+	}
+	
+	return _Steam_BGetCallback(hSteamPipe, pCallbackMsg);
+}
+
+SB_API void __cdecl Steam_FreeLastCallback2(HSteamPipe hSteamPipe) {
+	static FreeLastCallback_t _Steam_FreeLastCallback = NULL;
+	if(!_Steam_FreeLastCallback) {
+        _Steam_FreeLastCallback = reinterpret_cast<FreeLastCallback_t>(dlsym(RTLD_DEFAULT, "Steam_FreeLastCallback"));
+            
+        if(!_Steam_FreeLastCallback) {
+            fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_FreeLastCallback\n");
+            return;
+        }
+	}
+    
+	return _Steam_FreeLastCallback(hSteamPipe);
+}
+
+SB_API bool __cdecl Steam_GetAPICallResult2(HSteamPipe hSteamPipe, SteamAPICall_t hSteamAPICall, void* pCallback, int cubCallback, int iCallbackExpected, bool* pbFailed) {
+	static GetAPICallResult_t _Steam_GetAPICallResult = NULL;
+	if(!_Steam_GetAPICallResult)
+	{
+        _Steam_GetAPICallResult = reinterpret_cast<GetAPICallResult_t>(dlsym(RTLD_DEFAULT, "Steam_GetAPICallResult"));
+		
+        if(!_Steam_GetAPICallResult) {
+            fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_GetAPICallResult\n");
+            return false;
+		}
+	}
+	
+	return _Steam_GetAPICallResult(hSteamPipe, hSteamAPICall, pCallback, cubCallback, iCallbackExpected, pbFailed);
+}
+
+#elif defined( __linux__ )
+
+#include <dlfcn.h> // dlopen, dlsym, dlclose
+#include <stdio.h> // fprintf
+#include <stdlib.h> // getenv
+#include <limits.h> // PATH_MAX
+
+static char s_resolved_path[PATH_MAX];
+static const char* GetSteamClientPath() {
+	static bool _once = false;
+	if(!_once) {
+		int cx = snprintf(s_resolved_path, PATH_MAX, "%s/.steam/sdk32/steamclient.so", getenv("HOME"));
+		if(cx < 0 || cx > PATH_MAX) {
+			fprintf(stderr, "[CSteamworks] snprintf failed\n");
+		}
+		_once = true;
+	}
+	return s_resolved_path;
+}
+
+SB_API bool Steam_BGetCallback2(HSteamPipe hSteamPipe, CallbackMsg_t *pCallbackMsg) {
+	static BGetCallback_t _Steam_BGetCallback = NULL;
+	if(!_Steam_BGetCallback) {
+		void* hSteamClient = dlopen(GetSteamClientPath(), RTLD_LAZY);
+		if(hSteamClient) {
+			_Steam_BGetCallback = reinterpret_cast<BGetCallback_t>(dlsym(hSteamClient, "Steam_BGetCallback"));
+			dlclose(hSteamClient);
+
+			if(!_Steam_BGetCallback) {
+				fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_BGetCallback\n");
+				return false;
+			}
+		}
+		else {
+			fprintf(stderr, "[CSteamworks] dlopen failed opening steamclient.so\n");
+			return false;
+		}
+	}
+	
+	return _Steam_BGetCallback(hSteamPipe, pCallbackMsg);
+}
+
+SB_API void __cdecl Steam_FreeLastCallback2(HSteamPipe hSteamPipe) {
+	static FreeLastCallback_t _Steam_FreeLastCallback = NULL;
+	if(!_Steam_FreeLastCallback) {
+			void* hSteamClient = dlopen(GetSteamClientPath(), RTLD_LAZY);
+			if(hSteamClient) {
+				_Steam_FreeLastCallback = reinterpret_cast<FreeLastCallback_t>(dlsym(hSteamClient, "Steam_FreeLastCallback"));
+				dlclose(hSteamClient);
+
+				if(!_Steam_FreeLastCallback) {
+					fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_FreeLastCallback\n");
+					return;
+				}
+			}
+			else {
+				fprintf(stderr, "[CSteamworks] dlopen failed opening steamclient.so\n");
+				return;
+			}
+	}
+
+	return _Steam_FreeLastCallback(hSteamPipe);
+}
+
+SB_API bool __cdecl Steam_GetAPICallResult2(HSteamPipe hSteamPipe, SteamAPICall_t hSteamAPICall, void* pCallback, int cubCallback, int iCallbackExpected, bool* pbFailed) {
+	static GetAPICallResult_t _Steam_GetAPICallResult = NULL;
+	if(!_Steam_GetAPICallResult)
+	{
+		void* hSteamClient = dlopen(GetSteamClientPath(), RTLD_LAZY);
+		if(hSteamClient) {
+			_Steam_GetAPICallResult = reinterpret_cast<GetAPICallResult_t>(dlsym(hSteamClient, "Steam_GetAPICallResult"));
+			dlclose(hSteamClient);
+
+			if(!_Steam_GetAPICallResult) {
+				fprintf(stderr, "[CSteamworks] dlsym failed to find Steam_GetAPICallResult\n");
+				return false;
+			}
+		}
+		else {
+			fprintf(stderr, "[CSteamworks] dlopen failed opening steamclient.so\n");
+			return false;
+		}
+	}
+	
+	return _Steam_GetAPICallResult(hSteamPipe, hSteamAPICall, pCallback, cubCallback, iCallbackExpected, pbFailed);
+}
+#endif
